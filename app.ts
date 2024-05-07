cf3692f25f0cf9ec37ceca3cae3949c06a8fe70b
mhasnain23 <tryfrelancer23@gmail.com>
2024-05-06 23:45:31 -0700

    nig

--- app.ts
+++ app.ts
@@ -1,5 +1,12 @@
 
-for(let i = 0; i <= 50; i++){
-    const task =  "Hello, world";
-    console.log(task);
+// for(let i = 1; i <= 1000; ++i){ /* <-- this is called increment --> */
+//     let arr1: string[] = ["thank you kamran tessori"]
+//     console.log(arr1);
+// }
+
+
+const cars:string[] = ["Rolls Royce", "Bently", "F1", "R35 skyline"];
+
+for (let i = 0; i <= 4; i++) {
+    console.log("our cars name " + cars[i]);
 }
